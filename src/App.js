15b1536607dee545342c7c5dbaf6dff467dd2df7
topspinppy <5806021623049@fitm.kmutnb.ac.th>
2018-11-15 15:39:25 +0700

    initial Project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import { Spin } from 'antd'
 
-class App extends Component {
+
+class FakeLoading extends Component {
+  state = {
+    isLoading: true,
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ isLoading: false })
+    }, this.props.time)
+  }
   render() {
+    const { children } = this.props
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        {
+          this.state.isLoading === true ? (
+            <LandingPage>
+              <Spin tip="Loading ..." size="large" />
+            </LandingPage>
+          ) : (
+            children
+          )
+        }
+      </Fragment>
     );
   }
 }
 
-export default App;
+export default FakeLoading;
